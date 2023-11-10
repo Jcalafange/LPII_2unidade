d1b69e5703567697d39cdf96e70104c7f0a8a524
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2023-11-10 15:57:38 -0300

    asdasdas

--- controle/GeradorImpostoRenda.java
+++ controle/GeradorImpostoRenda.java
@@ -1,20 +1,15 @@
 package controle;
-
 import modelo.Pessoa;
-
 public class GeradorImpostoRenda {
     private double totalTributo;
-
     public GeradorImpostoRenda() {
-        this.totalTributo = 0.0;
+      super();
+      this.totalTributo = 0.0;
     }
-
     public double calculaValorTotalTributo(Pessoa p) {
-
 		this.totalTributo = p.calcularTributos();
 		this.totalTributo += p.getConta().calcularTributos();
 		this.totalTributo += p.getSeguro().calcularTributos();
-
 		return this.totalTributo;
     }
 }
